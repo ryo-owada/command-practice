b0a4fcc5783a076745fe33b1edad7c915055f5f6
Ryo Owada <r.oowada.sys21@morijyobi.ac.jp>
2022-03-17 11:43:31 +0900

    Main.javaの新規作成

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+		
+		Scanner stdIn = new Scanner(System.in);
+
+    		double height = stdIn.nextDouble();
+		double weight = stdIn.nextDouble();
+
+		double bmi = weight / Math.pow(height, 2);
+		
+		System.out.println(bmi);
+	}
+}
